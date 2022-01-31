1bbc37bc2905f541a4c84bc885face1ec00f6c70
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-31 18:56:10 +0100

    adds a button to get a new dog - wow!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import RandomDog from "./components/RandomDog.js";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <RandomDog />
+      <Link to="random-dog">Give me a random dog, please.</Link>
+      <Routes>
+        <Route path="/random-dog" element={<RandomDog />}/>
+      </Routes>
     </div>
   );
 }
--- src/components/RandomDog.js
+++ src/components/RandomDog.js
@@ -1,21 +1,32 @@
 import React from "react";
 import axios from "axios";
 
-const api = axios.create({baseURL: "https://dog.ceo/api/breeds/image"})
-
+const api = axios.create({ baseURL: "https://dog.ceo/api/breeds/image" });
 
 function RandomDog() {
-    async function getRandomDog() {
-        const {data} = await api.get("/random")
-        setRandomDog(data)
-    }
-    const [randomDog, setRandomDog] = React.useState({})
-    React.useEffect(()=>{
-        getRandomDog()
-    },[])
-  return <div>
-    <img src={randomDog.message}></img>
-    <em>Woof</em>
-  </div>;
+  //api call to db
+  async function getRandomDog() {
+    const { data } = await api.get("/random");
+    setRandomDog(data);
+  }
+  //states
+  const [randomDog, setRandomDog] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+  // use effect for api call
+  React.useEffect(() => {
+    getRandomDog();
+    setLoading(false)
+  }, []);
+
+  return (
+    <div className="randomDog">
+    {loading && <em>Throughing stick ...</em>}
+      <img src={randomDog.message} alt=""></img>
+      <br />
+      <em>Woof</em>
+      <br />
+      <button onClick={()=>getRandomDog()}>Throw another toy!</button>
+    </div>
+  );
 }
 export default RandomDog;
